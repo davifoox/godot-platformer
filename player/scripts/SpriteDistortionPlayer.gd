8ef4af37ef8c76ee9afc29005ac851733d3eee24
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-03-23 02:12:40 -0300

    refactoring

--- player/scripts/SpriteDistortionPlayer.gd
+++ player/scripts/SpriteDistortionPlayer.gd
@@ -1,8 +1,6 @@
 extends AnimationPlayer
 
-export(PackedScene) var dust_particle
-export(float) var dust_particle_y_position = 7
-onready var world = get_tree().get_root()
+
 var direction: int = 1
 
 func _on_StateMachine_transitioned(state_name, previous_state_name):
@@ -36,10 +34,3 @@ func play_animation(animation_name: String):
 func _on_Player_direciton_changed(dir):
 	if dir != 0:
 		direction = dir
-
-func spawn_dust_particle():
-	var dp = dust_particle.instance()
-	world.add_child(dp)
-	dp.global_position = owner.global_position
-	dp.global_position.y += dust_particle_y_position
-	dp.scale.x = direction
