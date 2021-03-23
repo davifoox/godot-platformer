7765a00f4dbf7c72e2ae5a251548a2254ff7ae7c
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-03-23 14:22:14 -0300

    Update Swing.gd

--- player/states/Swing.gd
+++ player/states/Swing.gd
@@ -1,19 +1,41 @@
 class_name Swing
 extends PlayerState
 
+var swing_impulse = 100
+
+func enter(_msg := {}) -> void:
+	pass
+
 func physics_update(delta: float) -> void:
 	#apply gravity
 	player.velocity.y += player.gravity * delta
 	player.velocity.y = clamp(player.velocity.y, -player.gravity, player.max_y_velocity)
 	
-	var radius = player.global_position - player.hook.position # points away from center
-	#if velocity.length() < 0.01 or radius.length() < 10: return
+	
+	var radius = player.global_position - player.hook.position
 	var angle = acos(radius.dot(player.velocity) / (radius.length() * player.velocity.length()))
 	var rad_velocity = cos(angle) * player.velocity.length()
+	#lerp(player.velocity, Vector2.ZERO, 1) tentando colocar uma fircção que pare de ficar balançando
+	
 	player.velocity += radius.normalized() * -rad_velocity
-	player.velocity += (player.hook.position - player.global_position).normalized() * player.retract_force * delta
+	player.velocity += (player.hook.position - player.global_position).normalized() * delta# * player.retract_force * delta
+	
+	if Input.is_action_pressed("left"):
+		player.velocity.x -= swing_impulse * delta
+	elif Input.is_action_pressed("right"):
+		player.velocity.x += swing_impulse * delta
+	
+	var max_x_velocity = 150
+	player.velocity.x = clamp(player.velocity.x, -max_x_velocity, max_x_velocity)
+	
 	if player.velocity.x != 0:
 		player.move_direction = sign(player.velocity.x)
+		
+	if Input.is_action_just_pressed("down"):
+		player.velocity.y = 0
 	
-	if Input.is_action_just_released("action1"):
+	
+	if Input.is_action_just_pressed("action1"):
+		state_machine.transition_to("Jump")
+	if Input.is_action_just_pressed("down"):
 		state_machine.transition_to("Fall")
