36c38526908e684a859682c2e007e8c8952e62da
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-03-23 15:50:40 -0300

    Update Swing.gd

--- player/states/Swing.gd
+++ player/states/Swing.gd
@@ -3,6 +3,7 @@ extends PlayerState
 
 var swing_impulse = 100
 var swing_friction = 25
+var max_x_velocity = 200
 
 func enter(_msg := {}) -> void:
 	pass
@@ -12,27 +13,23 @@ func physics_update(delta: float) -> void:
 	player.velocity.y += player.gravity * delta
 	player.velocity.y = clamp(player.velocity.y, -player.gravity, player.max_y_velocity)
 	
-	
 	var radius = player.global_position - player.hook.position
 	var angle = acos(radius.dot(player.velocity) / (radius.length() * player.velocity.length()))
 	var rad_velocity = cos(angle) * player.velocity.length()
-	#lerp(player.velocity, Vector2.ZERO, 1) tentando colocar uma fircção que pare de ficar balançando
-	apply_friciton(delta)
-	
+
 	player.velocity += radius.normalized() * -rad_velocity
 	player.velocity += (player.hook.position - player.global_position).normalized() * delta# * player.retract_force * delta
 	
+	apply_friciton(delta)
 	if Input.is_action_pressed("left"):
 		player.velocity.x -= swing_impulse * delta
 	elif Input.is_action_pressed("right"):
 		player.velocity.x += swing_impulse * delta
 	
-	var max_x_velocity = 150
 	player.velocity.x = clamp(player.velocity.x, -max_x_velocity, max_x_velocity)
 	
 	if player.velocity.x != 0:
 		player.move_direction = sign(player.velocity.x)
-		
 		
 	if Input.is_action_just_pressed("down"):
 		player.velocity.y = 0
@@ -44,9 +41,9 @@ func physics_update(delta: float) -> void:
 		state_machine.transition_to("Fall")
 
 func apply_friciton(delta):
-	print(player.velocity.x)
+	#print(player.velocity.x)
 	
-	if abs(player.velocity.x) > 1:
+	if abs(player.velocity.x) > 0.5:
 		
 		if player.move_direction == 1 and !Input.is_action_pressed("right"):
 			player.velocity.x -= swing_friction * delta
