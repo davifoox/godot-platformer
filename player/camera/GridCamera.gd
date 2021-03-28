e0a7b9f4a7d5290f07d2e46c64813e619a05e395
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-03-27 23:25:31 -0300

    grid camera working

--- player/camera/GridCamera.gd
+++ player/camera/GridCamera.gd
@@ -1,3 +1,22 @@
 class_name GridCamera
 extends GameplayCamera
 
+onready var grid_size = get_viewport().size
+var grid_position = Vector2()
+export(NodePath) onready var target = get_node(target)
+
+func _ready():
+#	set_as_toplevel(true)
+	update_grid_position()
+
+func _physics_process(delta):
+	update_grid_position()
+
+func update_grid_position():
+	var x = floor(target.global_position.x/ grid_size.x)
+	var y = floor(target.global_position.y / grid_size.y)
+	var new_grid_position = Vector2(x,y)
+	if grid_position == new_grid_position:
+		return
+	grid_position = new_grid_position
+	global_position = grid_position * grid_size
